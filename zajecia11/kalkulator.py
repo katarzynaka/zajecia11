30e1b5f88b322a31d91a9ac540212ed15fbd59db
katarzynaka <kasia.kozera@gmail.com>
2014-05-14 10:47:45 +0200

    dodanie funkcji getinfo i zintegrowanie galezi

--- zajecia11/kalkulator.py
+++ zajecia11/kalkulator.py
@@ -2,3 +2,9 @@ def dodawanie():
     a=int(input('dodaj pierwsza liczbe'))
     b=int(input('dodaj druga liczbe'))
     print(a+b)
+    
+def get_info():
+    print("ten program wywoluje")
+    
+get_info()
+dodawanie()
